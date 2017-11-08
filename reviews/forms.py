c2f3ac9e5333f28a136dd5dd3e3424c08e964170
root <zhou zhixing>
2017-11-08 16:34:36 +0800

    update 1108

---
+++ reviews/forms.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from django.forms import ModelForm, Textarea
+from reviews.models import Review
+
+
+class ReviewForm(ModelForm):
+    # 表单的内部类 Meta
+    class Meta:
+        model = Review
+        fields = ['user_name', 'rating', 'comment']     # 表单显示字段
+        widgets = {
+            'comment': Textarea(attrs={'cols': 40, 'rows': 15}),    # 重写字段的控件属性
+        }
--- reviews/urls.py
+++ reviews/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.review_list, name='review_list'),
     url(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
     url(r'^wine$', views.wine_list, name='wine_list'),
-    url(r'^wine/(?P<wine_id>[0-9])', views.wine_detail, name='wine_detail'),
+    url(r'^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
+    url(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
 ]
